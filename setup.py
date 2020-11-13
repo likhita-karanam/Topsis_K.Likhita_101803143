3fa7c216d2b947958e7c589e4b8482297a177aa6
likhita-karanam <67328252+likhita-karanam@users.noreply.github.com>
2020-11-13 16:59:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 
 from distutils.core import setup
 setup(
-  name = 'Topsis_101703297_thapar',         # How you named your package folder (MyLib)
-  packages = ['Topsis_101703297_thapar'],   # Chose the same as "name"
+  name = 'Topsis_k.likhita_101803143',         # How you named your package folder (MyLib)
+  packages = ['Topsis_k.likhita_101803143'],   # Chose the same as "name"
   version = '0.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Topsis implementation as a part of coursework of ucs633 course at thapar university',   # Give a short description about your library
-  author = 'Kunal bajaj',                   # Type in your name
-  author_email = 'kanchitbajaj8070@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/kanchitbajaj8070/Topsis_101703297_thapar.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/kanchitbajaj8070/Topsis_101703297_thapar/archive/v_02.tar.gz',    # I explain this later on
-  keywords = ['Topsis', 'Order Preference Similarity to Ideal Solutions', ' Rank','UCS633'],   # Keywords that define your package best
+  author = 'K Likhita',                   # Type in your name
+  author_email = 'karanamlikhita2000@gmail.com',      # Type in your E-Mail
+  url = 'https://github.com/likhita-karanam/Topsis_K.Likhita_101803143.git',   # Provide either the link to your github or to your wekeywords = ['Topsis', 'Order Preference Similarity to Ideal Solutions', ' Rank','UCS633'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
           'pandas','sys'
